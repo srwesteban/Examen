8160a4b1628d2c1d459dbae6b2bdd40865364a1f
William Esteban <william.esteban@campusucc.edu.co>
2022-09-13 10:48:13 -0500

    examen

---
+++ ClaseEmpleado/ClaseEmpleado/Empleado.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseEmpleado
+{
+    internal class Empleado
+    {
+        public string nombre;
+        public string id;
+        public int sueldo;
+        public int horas;
+
+
+        public Empleado(string a, string b, int c, int d)
+        {
+            this.nombre = a;
+            this.id = b;
+            this.sueldo = c;
+            this.horas = d;
+        }
+        public int retornarSalario(int a)
+        {
+            int salario = sueldo * a;
+            sueldo = salario;
+            return salario;
+        }
+        public int incrementar()
+        {
+            
+            if(sueldo <= 1500000)
+            {
+                sueldo += 100000;
+            }
+            return sueldo;
+        }
+        public int horasExtra()
+        {
+
+            if (horas > 6)
+            {
+                sueldo += 5000;
+            }
+            return sueldo;
+        }
+
+        public override string ToString()
+        {
+            return $"El sueldo del empleado {nombre} es de {sueldo}";
+        }
+
+
+    }
+}
---
+++ ClaseEmpleado/ClaseEmpleado/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ClaseEmpleado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sueldo;
+            string nombre;
+            string id;
+            int horas;
+            string opcion;
+            Console.WriteLine("ingrese un nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese su id");
+            id = Console.ReadLine();
+            Console.WriteLine("ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese las horas de trabajo");
+            horas = Convert.ToInt32(Console.ReadLine());
+
+            Empleado empleado = new Empleado(nombre, id, sueldo, horas);
+
+            empleado.retornarSalario(horas);
+            Console.WriteLine("Desa hacer un incremento de salario si/no");
+            opcion = Console.ReadLine();
+            if(opcion == "si")
+            {
+                empleado.incrementar();
+
+            }
+            empleado.horasExtra();
+            Console.WriteLine(empleado);
+        }
+    }
+}
